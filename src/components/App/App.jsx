2d8abe61f1ab5c2156ad8ba6aa004400e0a3d422
Appili Vamsi Krishna <appilivamsikrishna@gmail.com>
2023-06-04 03:18:13 +0530

    1st commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import UberPage from "../UberPage/UberPage";
+import data from "../../data";
+
+function App() {
+  const defaultItem = {
+    link: "https://apps.appili.dev",
+    description: "View my mobile Apps",
+    background_logo: "box6.png",
+    background_logo_size: "16%",
+    background_picture: "appleApp.jpg",
+    background_picture_size: "cover",
+    background_color: "#FFFFFF",
+  };
+  if (data.settings.default_item === undefined || data.settings.default_item) {
+    data.items.push(defaultItem);
+  }
+  return (
+    <div className="App">
+      <UberPage items={data.items} settings={data.settings} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,45 @@
+export default {
+  items: [
+    // {
+    //   link: "https://github.com/StegSchreck", // optional
+    //   title: "https://github.com/StegSchreck", // optional
+    //   description: "My open source projects on GitHub", // optional
+    //   text_color: "#ffffff", // defaults to '#4A86E8' - affects both title and description (if set)
+    //   background_logo: "GitHub_white.svg", // optional
+    //   background_logo_size: "70%", // defaults to '50%'
+    //   background_picture: "cover.jpg", // optional
+    //   background_picture_size: "cover", // defaults to 'cover'
+    //   background_color: "#000000", // defaults to '#282c34'
+    // },
+    {
+      link: "https://projects.appili.dev",
+      description: "View my Projects",
+      background_picture: "projectsz.jpg",
+      background_picture_size: "cover",
+      background_logo: "bulb.png",
+      background_logo_size: "40%",
+    },
+    {
+      link: "https://cv.appili.dev",
+      description: "View my Résumé",
+      background_logo: "Angular_full_color_logo.svg",
+      background_logo_size: "25%",
+      background_picture: "angularArch.jpg",
+      background_picture_size: "cover",
+      background_color: "#FFFFFF",
+    },
+    {
+      link: "https://swagger.appili.dev",
+      text_color: "#FFFFFF",
+      description: "Try my REST APIs",
+      background_logo: "API.png",
+      background_logo_size: "60%",
+      background_picture: "aws.jpg",
+      background_picture_size: "cover",
+      background_color: "#FFFFFF",
+    },
+  ],
+  settings: {
+    default_link_target: "_blank",
+  },
+};
