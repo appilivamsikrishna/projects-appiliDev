01597b62df009994b5f6265d8a4e1ba2d146f634
Krishna <appilivamsikrishna@gmail.com>
2023-06-07 08:23:43 +0530

    projects

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,13 +5,14 @@ import data from "../../data";
 
 function App() {
   const defaultItem = {
-    link: "https://apps.appili.dev",
-    description: "View my mobile Apps",
-    background_logo: "box6.png",
-    background_logo_size: "16%",
-    background_picture: "appleApp.jpg",
+    link: "https://sftp-status.appili.io/",
+    text_color: "#FFFFFF",
+    description: "status of SFTP pipeline",
+    background_logo: "sftp-status.png",
+    background_logo_size: "20%",
+    background_picture: "",
     background_picture_size: "cover",
-    background_color: "#FFFFFF",
+    background_color: "#74ee15",
   };
   if (data.settings.default_item === undefined || data.settings.default_item) {
     data.items.push(defaultItem);
--- src/components/UberTile/UberTile.jsx
+++ src/components/UberTile/UberTile.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './UberTile.css';
+import React from "react";
+import "./UberTile.css";
 
 class UberTile extends React.Component {
   render() {
@@ -47,34 +47,60 @@ class UberTile extends React.Component {
       ...gridStyle,
       lineHeight: `${itemHeight}vh`,
       marginTop: `-${itemHeight}vh`,
-      fontSize: `${itemHeight / 15}vh`,
+      fontSize: `${itemHeight / 12}vh`,
     };
 
-    if (item.background_color) { backgroundImageStyle.backgroundColor = item.background_color; }
-    if (item.text_color) { titleStyle.color = item.text_color; }
+    if (item.background_color) {
+      backgroundImageStyle.backgroundColor = item.background_color;
+    }
+    if (item.text_color) {
+      titleStyle.color = item.text_color;
+    }
     if (item.background_logo) {
       backgroundImageStyle.backgroundImage = `url(/img/${item.background_logo})`;
-      backgroundImageStyle.backgroundSize = item.background_logo_size ? item.background_logo_size : '50%';
+      backgroundImageStyle.backgroundSize = item.background_logo_size
+        ? item.background_logo_size
+        : "50%";
     }
     if (item.background_picture) {
-      let backgroundImagePrefix = '';
-      let backgroundSizePrefix = '';
+      let backgroundImagePrefix = "";
+      let backgroundSizePrefix = "";
       if (item.background_logo) {
         backgroundImagePrefix = `${backgroundImageStyle.backgroundImage}, `;
         backgroundSizePrefix = `${backgroundImageStyle.backgroundSize}, `;
       }
       backgroundImageStyle.backgroundImage = `${backgroundImagePrefix}url(/img/${item.background_picture})`;
-      const backgroundSizeSuffix = item.background_picture_size ? item.background_picture_size : 'cover';
+      const backgroundSizeSuffix = item.background_picture_size
+        ? item.background_picture_size
+        : "cover";
       backgroundImageStyle.backgroundSize = `${backgroundSizePrefix}${backgroundSizeSuffix}`;
     }
-    if (item.title && (item.background_logo || item.background_picture)) { backgroundImageStyle.opacity = '0.2'; }
+    if (item.title && (item.background_logo || item.background_picture)) {
+      backgroundImageStyle.opacity = "0.2";
+    }
 
     return (
-      <div className={`UberTile${item.link ? ' withLink' : ''}`} style={tileStyle}>
-        <a className="UberTile-link" {... item.link ? {href: item.link} : {}} target={settings.default_link_target}>
-          <div className="UberTile-background-image" style={backgroundImageStyle} />
-          {item.description && <div className="UberTile-description" style={descriptionStyle}><span>{item.description}</span></div>}
-          <div className="UberTile-title" style={titleStyle}><span>{item.title}</span></div>
+      <div
+        className={`UberTile${item.link ? " withLink" : ""}`}
+        style={tileStyle}
+      >
+        <a
+          className="UberTile-link"
+          {...(item.link ? { href: item.link } : {})}
+          target={settings.default_link_target}
+        >
+          <div
+            className="UberTile-background-image"
+            style={backgroundImageStyle}
+          />
+          {item.description && (
+            <div className="UberTile-description" style={descriptionStyle}>
+              <span>{item.description}</span>
+            </div>
+          )}
+          <div className="UberTile-title" style={titleStyle}>
+            <span>{item.title}</span>
+          </div>
         </a>
       </div>
     );
