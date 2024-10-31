220d0deca3e7774ff3e97dbb08f729df98902275
Krishna <appilivamsikrishna@gmail.com>
2024-10-31 16:11:57 +0530

    mobile-apps

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,7 @@ import data from "../../data";
 
 function App() {
   const defaultItem = {
-    link: "https://apps.appili.dev/",
+    link: "https://mobile-apps.appili.dev/",
     description: "View my Mobile Apps",
     background_logo: "box6.png",
     background_logo_size: "16%",
